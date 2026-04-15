6b4d8347fc1e73560065f0d3a35aa47fdf0b9b06
rudresh-sharma <rudresh.sharam.8602@gmail.com>
2026-04-15 20:35:18 +0530

    [Rudresh] : Update the environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://api-gateway-slc7.onrender.com',
   frontendUrl: 'https://quantitymeasurementapp-frontend-ysu8.onrender.com',
-  googleAuthUrl: 'https://api-gateway-slc7.onrender.com/oauth2/authorization/google',
+  googleAuthUrl: 'https://authentication-service-wjg4.onrender.com/oauth2/authorization/google',
   googleSuccessPath: '/oauth-success'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiBaseUrl: '',
   frontendUrl: 'http://localhost:4200',
-  googleAuthUrl: 'https://api-gateway-slc7.onrender.com/oauth2/authorization/google',
+  googleAuthUrl: 'https://authentication-service-wjg4.onrender.com/oauth2/authorization/google',
   googleSuccessPath: '/oauth-success'
 };

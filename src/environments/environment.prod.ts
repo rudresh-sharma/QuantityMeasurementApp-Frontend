4440ce417da3c10a555073a9f5e4329a74d8f977
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-07-22 22:42:38 +0530

    [RUdresh] : Added Correct Render backend url to frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiBaseUrl: '',
   frontendUrl: 'https://quantitymeasurementapp-frontend-ysu8.onrender.com',
-  googleAuthUrl: '/oauth2/authorization/google',
+  googleAuthUrl: 'https://quantitymeasurementapp-1-ev12.onrender.com/oauth2/authorization/google',
   googleSuccessPath: '/oauth-success'
 };
